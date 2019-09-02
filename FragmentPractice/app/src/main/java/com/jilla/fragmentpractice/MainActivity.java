e48639c236b2b626791e5d57ca71848054f77cd9
Vikas Jilla <jillavikas@gmail.com>
2019-09-02 19:52:02 +0530

    added fragment transactions, backstack

--- FragmentPractice/app/src/main/java/com/jilla/fragmentpractice/MainActivity.java
+++ FragmentPractice/app/src/main/java/com/jilla/fragmentpractice/MainActivity.java
@@ -1,11 +1,19 @@
 package com.jilla.fragmentpractice;
 
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final String KEY_BTN = "BTN_TXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,58 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.containerToAddFragmentProgramaticaly,fragment,"optionalTag");
         transaction.commit();
+        transaction.addToBackStack("add Transaction");
+    }
+
+    public void replaceFragment(View view) {
+//        SampleFragment fragment = new SampleFragment();
+        FragmentManager manager = getSupportFragmentManager();//using support fragment manager as we used Support Fragment class
+        if(view.getTag() == null) {
+            //while implementing(extends Fragment) SampleFragment class
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.detach(manager.findFragmentByTag("optionalTag"));
+            transaction.commit();
+            view.setTag("hehe");
+            Button btn = (Button) view;
+            btn.setText("hellooooo");
+            transaction.addToBackStack("detach Transaction");
+        }
+        else{
+//            tends Fragment) SampleFragment class
+            Fragment fragment = manager.findFragmentByTag("optionalTag");
+            Bundle arguments = new Bundle();
+            arguments.putString("BUNDLE_ARG","hello");
+            fragment.setArguments(arguments);
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.attach(manager.findFragmentByTag("optionalTag"));
+            transaction.commit();
+            transaction.addToBackStack("attach Transaction");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(KEY_BTN,"hellooooo");
+        super.onSaveInstanceState(outState);
+        Log.e(TAG,"onSaveInstanceState");
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(getSupportFragmentManager().getBackStackEntryCount() > 0){
+            String transactionName = getSupportFragmentManager().getBackStackEntryAt(getSupportFragmentManager().getBackStackEntryCount()-1).getName();
+            ((Button)findViewById(R.id.button)).setText(transactionName);
+            getSupportFragmentManager().popBackStack();
+        }else super.onBackPressed();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e(TAG,"onRestoreInstanceState");
+        String val = (String) savedInstanceState.getString(KEY_BTN);
+        Button btn = findViewById(R.id.button);
+        btn.setText(val);
+
     }
 }
--- FragmentPractice/app/src/main/java/com/jilla/fragmentpractice/SampleFragment.java
+++ FragmentPractice/app/src/main/java/com/jilla/fragmentpractice/SampleFragment.java
@@ -1,19 +1,96 @@
 package com.jilla.fragmentpractice;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.zip.Inflater;
 
 public class SampleFragment extends Fragment {
 
+    private static final String TAG = SampleFragment.class.getSimpleName();
+
+    TextView textView;
+
+    @Override
+    public void onAttach(Context ctx){
+        super.onAttach(ctx);
+        Log.e(TAG,"onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle state){
+        super.onCreate(state);
+        Log.e(TAG,"onCreate");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
         View view = inflater.inflate(R.layout.sample_fragment,container,false);
+        Log.e(TAG,"onCreateView");
+        textView = view.findViewById(R.id.textView);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle state){
+        super.onActivityCreated(state);
+        Bundle bd = getArguments();
+        if(state != null || bd != null) {
+            bd = state != null?state:bd;
+            String s = bd.getString("BUNDLE_ARG", "test");
+            textView.setText(s);
+        }
+        Log.e(TAG,"onActivityCreated");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle){
+        super.onSaveInstanceState(bundle);
+        bundle.putString("BUNDLE_ARG","test");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.e(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.e(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.e(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.e(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.e(TAG,"onDestroy");
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        Log.e(TAG,"onDetach");
     }
 
 }
